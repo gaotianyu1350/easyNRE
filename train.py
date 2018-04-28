75a83e1656f9b313c05c0686f1458eb217d770ea
Jason Yu <yuzhijingcheng1996@hotmail.com>
2018-04-28 09:15:56 +0800

    Wrong flag types fixed.

--- train.py
+++ train.py
@@ -16,24 +16,24 @@ config_file = open(os.path.join(FLAGS.export_path, "config"), 'r')
 config = json.loads(config_file.read())
 config_file.close()
 
-tf.app.flags.DEFINE_float('max_length', config['fixlen'], 'maximum of number of words in one sentence')
-tf.app.flags.DEFINE_float('pos_num', config['maxlen'] * 2 + 1, 'number of position embedding vectors')
-tf.app.flags.DEFINE_float('num_classes', len(config['relation2id']),'maximum of relations')
+tf.app.flags.DEFINE_integer('max_length', config['fixlen'], 'maximum of number of words in one sentence')
+tf.app.flags.DEFINE_integer('pos_num', config['maxlen'] * 2 + 1, 'number of position embedding vectors')
+tf.app.flags.DEFINE_integer('num_classes', len(config['relation2id']),'maximum of relations')
 
-tf.app.flags.DEFINE_float('hidden_size',230,'hidden feature size')
-tf.app.flags.DEFINE_float('pos_size',5,'position embedding size')
-tf.app.flags.DEFINE_float('word_size', 50, 'word embedding size')
+tf.app.flags.DEFINE_integer('hidden_size',230,'hidden feature size')
+tf.app.flags.DEFINE_integer('pos_size',5,'position embedding size')
+tf.app.flags.DEFINE_integer('word_size', 50, 'word embedding size')
 
-tf.app.flags.DEFINE_float('max_epoch',60,'maximum of training epochs')
-tf.app.flags.DEFINE_float('batch_size',160,'entity numbers used each training time')
+tf.app.flags.DEFINE_integer('max_epoch',60,'maximum of training epochs')
+tf.app.flags.DEFINE_integer('batch_size',160,'entity numbers used each training time')
 tf.app.flags.DEFINE_float('learning_rate',0.5,'entity numbers used each training time')
 tf.app.flags.DEFINE_float('weight_decay',0.00001,'weight_decay')
 tf.app.flags.DEFINE_float('keep_prob',0.7,'dropout rate')
 
 tf.app.flags.DEFINE_string('model_dir','./checkpoint/','path to store model')
 tf.app.flags.DEFINE_string('summary_dir','./summary','path to store summary_dir')
-tf.app.flags.DEFINE_string('use_adv', False, 'use adversarial training or not')
-tf.app.flags.DEFINE_string('save_epoch', 2, 'save the checkpoint after how many epoches')
+tf.app.flags.DEFINE_boolean('use_adv', False, 'use adversarial training or not')
+tf.app.flags.DEFINE_integer('save_epoch', 2, 'save the checkpoint after how many epoches')
 
 from framework import Framework 
 def main(_):
